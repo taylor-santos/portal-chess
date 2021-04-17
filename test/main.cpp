cef8695434f66c4593564a2ce1a2d52418ed4df4
taylor-santos <taylor.p.santos@gmail.com>
2021-04-17 16:02:16 -0700

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by taylor-santos on 4/17/2021 at 15:46.
+//
+
+#include "gtest/gtest.h"
+
+int
+main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ test/sample.cpp
@@ -0,0 +1,9 @@
+//
+// Created by taylor-santos on 4/17/2021 at 13:20.
+//
+
+#include "gtest/gtest.h"
+
+TEST(SampleTest, True) {
+    ASSERT_TRUE(true);
+}
