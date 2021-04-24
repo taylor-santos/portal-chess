1c708ea56cd30bef1c4297a6b3ebc9d2d78e90de
Taylor Santos <taylor.p.santos@gmail.com>
2021-04-23 19:40:56 -0700

    Add immutable chess board

--- include/board.h
+++ include/board.h
@@ -6,12 +6,14 @@
 #define PORTAL_CHESS_INCLUDE_BOARD_H
 
 #include <vector>
+#include <iostream>
 #include <memory>
 #include <optional>
 
 namespace Chess {
 
 enum class Color { White, Black };
+enum class Type { Bishop, King, Knight, Pawn, Portal, Queen, Rook };
 
 // Use enum, not enum class, to allow implicit int conversion
 enum File { A = 1, B, C, D, E, F, G, H };
@@ -21,9 +23,17 @@ std::ostream &
 operator<<(std::ostream &os, File file);
 
 struct Coord {
+    /***
+     * Construct a Coord instance from the given file and rank values.
+     * @param file the horizontal coordinate, letters A-H representing values [1,8]
+     * @param rank the vertical coordinate, representing values [1,8]
+     * @throws std::invalid_argument if either argument is outside of the interval [1,8]
+     */
     Coord(File file, Rank rank);
+
     bool
     operator==(const Coord &other) const;
+
     bool
     operator!=(const Coord &other) const;
 
@@ -35,78 +45,83 @@ std::ostream &
 operator<<(std::ostream &os, const Coord &coord);
 
 struct Piece {
-    enum class Type { Bisop, King, Knight, Pawn, Portal, Queen, Rook } type;
+    Type  type;
     Color color;
-    Coord coord;
 
     bool
     operator==(const Piece &other) const;
+
     bool
     operator!=(const Piece &other) const;
 };
 
-class Board {
+class Board : public std::enable_shared_from_this<Board> {
 public:
-    Board();
     Board(const Board &) = delete;
-    ~Board();
-
-    Board &
-    operator=(const Board &) = delete;
+    virtual ~Board()     = default;
 
     /***
-     * Retrieve an optional Piece from a given coordinate. If no piece exists, returns an empty
-     * std::optional.
-     * @param coord The Coord representing the rank and file to get a piece from
-     * @returns An std::optional containing a Piece if one exists, or an empty std::optional
-     *         otherwise
+     * Construct a new Board from a list of pieces and return it wrapped in an std::shared_ptr.
+     * @param pieces a list of (coord, piece) pairs to be added to the board
+     * @returns a newly constructed Board wrapped in a std::shared_ptr, containing the given pieces
+     * @throws invalid_piece if two or more of the given pieces have overlapping coordinates
      */
-    [[nodiscard]] std::optional<Piece>
-    getPiece(Coord coord) const;
+    [[nodiscard]] static std::shared_ptr<const Board>
+    make(const std::vector<std::pair<Coord, Piece>> &pieces);
 
     /***
-     * Inserts a piece onto the board. The placement is taken from the piece's coord field.
-     * @param piece the piece to be placed on the board
-     * @throws invalid_piece if the piece's coordinate is already occupied by an existing piece
+     * Retrieve a piece from the Board at the given coordinate.
+     * @param coord the coordinate to retrieve a piece from
+     * @returns an std::optional containing the piece at the given coordinate if one exists, or an
+     *          empty std::optional otherwise
      */
-    void
-    addPiece(Piece piece);
+    [[nodiscard]] virtual std::optional<Piece>
+    at(Coord coord) const = 0;
 
     /***
-     * Remove a piece from the board if it is at the given coordinate, do nothing otherwise.
+     * Construct a new Board representing this Board's state, plus an added piece.
+     * @param coord the coordinate to add the piece at
+     * @param piece the piece to add to the new Board
+     * @returns a new Board state representing this Board with the piece added
+     * @throws invalid_piece if this Board already has a piece at the new piece's coordinates
+     */
+    [[nodiscard]] std::shared_ptr<const Board>
+    addPiece(Coord coord, Piece piece) const;
+
+    /***
+     * Construct a new Board representing this Board's state, with one piece removed from the given
+     * coordinate.
      * @param coord the coordinate to remove a piece from
+     * @returns a new Board state representing this Board with a piece removed
+     * @throws invalid_piece if no piece exists at the given coordinate
      */
-    void
-    removePiece(Coord coord);
+    [[nodiscard]] std::shared_ptr<const Board>
+    removePiece(Coord coord) const;
 
-    /***
-     * Move a piece from one position to another. This must be invoked from an occupied coordinate
-     * to an unoccupied coordinate, otherwise an invalid_piece exception is thrown.
-     * @param from the coordinate to move a piece from
+    /**
+     * Construct a new Board representing this Board's state, with one piece moved from one
+     * coordinate to another.
+     * @param from the coordinate to move the piece from
      * @param to the coordinate to move the piece to
-     * @throws invalid_piece if moving from an unoccupied location or if moving to an occupied
-     *         location. If an exception is thrown, the board is guaranteed to be unchanged.
+     * @returns a new Board state representing this Board with one piece moved
+     * @throws invalid_piece if either the "from" coordinate is unoccupied,
+     *         or if the "to" coordinate is occupied
      */
-    void
-    movePiece(Coord from, Coord to);
+    [[nodiscard]] std::shared_ptr<const Board>
+    movePiece(Coord from, Coord to) const;
 
 private:
-    class BoardState {
-    public:
-        [[nodiscard]] const std::optional<Piece> &
-        operator()(Coord coord) const;
+    class InitialBoard;
+    class AddedPiece;
+    class RemovedPiece;
+    class MovedPiece;
 
-        [[nodiscard]] std::optional<Piece> &
-        operator()(Coord coord);
-
-    private:
-        std::optional<Piece> board[8][8];
-    } board_;
+    Board() = default;
 };
 
 class invalid_piece : public std::runtime_error {
 public:
-    invalid_piece(const std::string &arg);
+    explicit invalid_piece(const std::string &arg);
 };
 
 } // namespace Chess
--- src/board.cpp
+++ src/board.cpp
@@ -4,8 +4,8 @@
 
 #include "board.h"
 
-#include <algorithm>
 #include <sstream>
+#include <array>
 
 namespace Chess {
 
@@ -38,55 +38,9 @@ Coord::operator!=(const Coord &other) const {
     return !(*this == other);
 }
 
-std::ostream &
-operator<<(std::ostream &os, const Coord &coord) {
-    return os << coord.file << coord.rank;
-}
-
-Board::Board() = default;
-
-Board::~Board() = default;
-
-std::optional<Piece>
-Board::getPiece(Coord coord) const {
-    return board_(coord);
-}
-
-void
-Board::addPiece(Piece piece) {
-    if (getPiece(piece.coord)) {
-        std::stringstream ss;
-        ss << "Cannot add piece to " << piece.coord << ": this space is occupied";
-        throw invalid_piece(ss.str());
-    }
-    board_(piece.coord) = piece;
-}
-
-void
-Board::removePiece(Coord coord) {
-    board_(coord).reset();
-}
-
-void
-Board::movePiece(Coord from, Coord to) {
-    auto &optPiece = board_(from);
-    if (!optPiece) {
-        std::stringstream ss;
-        ss << "Cannot move piece from " << from << ": this space is empty";
-        throw invalid_piece(ss.str());
-    }
-    Piece newPiece = *optPiece;
-    newPiece.coord = to;
-    addPiece(newPiece);
-    optPiece.reset();
-}
-
-invalid_piece::invalid_piece(const std::string &arg)
-    : std::runtime_error(arg) {}
-
 bool
 Piece::operator==(const Piece &other) const {
-    return type == other.type && color == other.color && coord == other.coord;
+    return type == other.type && color == other.color;
 }
 
 bool
@@ -94,16 +48,183 @@ Piece::operator!=(const Piece &other) const {
     return !(*this == other);
 }
 
-const std::optional<Piece> &
-Board::BoardState::operator()(Coord coord) const {
-    auto [file, rank] = coord;
-    return board[file - 1][rank - 1];
+std::ostream &
+operator<<(std::ostream &os, const Coord &coord) {
+    return os << coord.file << coord.rank;
 }
 
-std::optional<Piece> &
-Board::BoardState::operator()(Coord coord) {
+class Board::InitialBoard : public Board {
+public:
+    explicit InitialBoard(const std::vector<std::pair<Coord, Piece>> &pieces);
+
+    std::optional<Piece>
+    at(Coord coord) const override;
+
+private:
+    class BoardState {
+    public:
+        explicit BoardState(std::array<std::array<std::optional<Piece>, 8>, 8> board);
+
+        [[nodiscard]] const std::optional<Piece> &
+        operator()(Coord coord) const;
+
+    private:
+        const std::array<std::array<std::optional<Piece>, 8>, 8> board_;
+    } board_;
+};
+
+class Board::AddedPiece : public Board {
+public:
+    AddedPiece(std::shared_ptr<const Board> board, Coord coord, Piece piece);
+
+    std::optional<Piece>
+    at(Coord coord) const override;
+
+private:
+    const std::shared_ptr<const Board> board_;
+    const Coord                        coord_;
+    const Piece                        piece_;
+};
+
+class Board::RemovedPiece : public Board {
+public:
+    RemovedPiece(std::shared_ptr<const Board> board, Coord coord);
+
+    std::optional<Piece>
+    at(Coord coord) const override;
+
+private:
+    const std::shared_ptr<const Board> board_;
+    const Coord                        coord_;
+};
+
+class Board::MovedPiece : public Board {
+public:
+    MovedPiece(std::shared_ptr<const Board> board, Coord from, Coord to);
+
+    std::optional<Piece>
+    at(Coord coord) const override;
+
+private:
+    const std::shared_ptr<const Board> board_;
+    const Coord                        from_;
+    const Coord                        to_;
+};
+
+std::shared_ptr<const Board>
+Board::make(const std::vector<std::pair<Coord, Piece>> &pieces) {
+    return std::make_unique<InitialBoard>(pieces);
+}
+
+std::shared_ptr<const Board>
+Board::addPiece(Coord coord, Piece piece) const {
+    return std::make_unique<AddedPiece>(shared_from_this(), coord, piece);
+}
+
+std::shared_ptr<const Board>
+Board::removePiece(Coord coord) const {
+    return std::make_unique<RemovedPiece>(shared_from_this(), coord);
+}
+
+std::shared_ptr<const Board>
+Board::movePiece(Coord from, Coord to) const {
+    return std::make_unique<MovedPiece>(shared_from_this(), from, to);
+}
+
+static std::array<std::array<std::optional<Piece>, 8>, 8>
+getPieces(const std::vector<std::pair<Coord, Piece>> &pieces) {
+    std::array<std::array<std::optional<Piece>, 8>, 8> board;
+    for (auto [coord, piece] : pieces) {
+        auto [file, rank] = coord;
+        auto &prev        = board[file - 1][rank - 1];
+        if (prev) {
+            std::stringstream ss;
+            ss << "Cannot add piece to " << coord << ": this space is occupied";
+            throw invalid_piece(ss.str());
+        } else {
+            prev = piece;
+        }
+    }
+    return board;
+}
+
+Board::InitialBoard::InitialBoard(const std::vector<std::pair<Coord, Piece>> &pieces)
+    : board_{getPieces(pieces)} {}
+
+std::optional<Piece>
+Board::InitialBoard::at(Coord coord) const {
+    return board_(coord);
+}
+
+Board::InitialBoard::BoardState::BoardState(
+    std::array<std::array<std::optional<Piece>, 8>, 8> board)
+    : board_{board} {}
+
+const std::optional<Piece> &
+Board::InitialBoard::BoardState::operator()(Coord coord) const {
     auto [file, rank] = coord;
-    return board[file - 1][rank - 1];
+    return board_[file - 1][rank - 1];
+}
+
+Board::AddedPiece::AddedPiece(std::shared_ptr<const Board> board, Coord coord, Piece piece)
+    : board_{std::move(board)}
+    , coord_{coord}
+    , piece_{piece} {
+    if (board_->at(coord)) {
+        std::stringstream ss;
+        ss << "Cannot add piece to " << coord << ": this space is occupied";
+        throw invalid_piece(ss.str());
+    }
+}
+
+std::optional<Piece>
+Board::AddedPiece::at(Coord coord) const {
+    return coord_ == coord ? piece_ : board_->at(coord);
+}
+
+Board::RemovedPiece::RemovedPiece(std::shared_ptr<const Board> board, Coord coord)
+    : board_{std::move(board)}
+    , coord_{coord} {
+    if (!board_->at(coord)) {
+        std::stringstream ss;
+        ss << "Cannot remove piece from " << coord << ": this space is empty";
+        throw invalid_piece(ss.str());
+    }
+}
+
+std::optional<Piece>
+Board::RemovedPiece::at(Coord coord) const {
+    return coord == coord_ ? std::nullopt : board_->at(coord);
+}
+
+invalid_piece::invalid_piece(const std::string &arg)
+    : std::runtime_error(arg) {}
+
+Board::MovedPiece::MovedPiece(std::shared_ptr<const Board> board, Coord from, Coord to)
+    : board_{std::move(board)}
+    , from_{from}
+    , to_{to} {
+    if (board_->at(to_)) {
+        std::stringstream ss;
+        ss << "Cannot move piece to " << to_ << ": this space is occupied";
+        throw invalid_piece(ss.str());
+    }
+    if (!board_->at(from_)) {
+        std::stringstream ss;
+        ss << "Cannot move piece from " << from_ << ": this space is empty";
+        throw invalid_piece(ss.str());
+    }
+}
+
+std::optional<Piece>
+Board::MovedPiece::at(Coord coord) const {
+    if (coord == to_) {
+        return board_->at(from_);
+    } else if (coord == from_) {
+        return std::nullopt;
+    } else {
+        return board_->at(coord);
+    }
 }
 
 } // namespace Chess
